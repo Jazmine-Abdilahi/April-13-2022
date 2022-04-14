d1db7f16d0dc92fee3fe8083d7e7dc85f06aa4d0
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-04-13 19:15:30 -0500

    Add files via upload

---
+++ WendsdayApril13.js
@@ -0,0 +1,47 @@
+// Wndsday April 13 
+// Video 95 - 96 - 97
+
+// Example 
+console.log(undefined);
+if(!numberProducts) deleteShopingCart();
+
+var numberProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!! :D')  
+  }
+  
+  var x = 1;
+  let y = 7;
+  const z = 3;
+
+  console.log(x === windows.x);
+  console.log(y === windows.y);
+  console.log(z === windows.z);
+  /*                                          */
+
+  const Jazmine = {
+      name: 'Jazmine',
+      year: '2010',
+      calcAge: function() {
+          return 2037 - this.year;
+      }
+  };
+  Jazmine.calcAge(); // 11
+
+  /*                                          */
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+    console.log(2037 - birthYear =>);
+    console.log(this);
+};
+calcAge(2010);
+
+const calcAgeArrow = birthYear => {
+    console.log(this: typeof globalThis)
+    console.log(this)
+};
+calcAgeArrow(2010);
+
